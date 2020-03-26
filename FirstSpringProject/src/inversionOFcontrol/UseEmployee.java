eb911af245e1b0ba7baf8ae60f833a8dae4eb4bf
Hector Ortega <kodeprestige@gmail.com>
2020-03-26 18:42:25 -0400

    *Installation of Spring libraries (Spring JAR files). *Add xml configuration file applicationContext.xml 	>Add the first configuration bean to create objects of type employee

--- FirstSpringProject/src/inversionOFcontrol/UseEmployee.java
+++ FirstSpringProject/src/inversionOFcontrol/UseEmployee.java
@@ -1,12 +1,23 @@
 package inversionOFcontrol;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class UseEmployee {
 
 	public static void main(String[] args) {
 		
-		Employee employee = new DirectorEmployee();
+		//load the xml file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
+		//ask for the bean
+		Employee employee = context.getBean("myEmployee", Employee.class);
+		
+		//close the xml file
+		context.close();
+		
+		//use the bean
 		System.out.println(employee.getType());
+		
 
 	}
 
