2643b25518bba4d700fb7072e6f239e2a50c6f0c
Hector Ortega <kodeprestige@gmail.com>
2020-03-28 21:13:09 -0400

    Java annotations:
    	-Singleton and Prototype
    		SecretaryEmployee -> @Scope("prototype")

--- FirstSpringProject/src/com/kodeprestige/inversionOFcontrol/SecretaryEmployee.java
+++ FirstSpringProject/src/com/kodeprestige/inversionOFcontrol/SecretaryEmployee.java
@@ -2,9 +2,12 @@ package com.kodeprestige.inversionOFcontrol;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+
+@Scope("prototype")
 public class SecretaryEmployee implements Employee {
 
 	
--- FirstSpringProject/src/com/kodeprestige/inversionOFcontrol/UseEmployee.java
+++ FirstSpringProject/src/com/kodeprestige/inversionOFcontrol/UseEmployee.java
@@ -8,17 +8,30 @@ public class UseEmployee {
 		
 		//Load the xml file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+ 		//Ask for the bean
+ 		Employee employee1 = context.getBean("secretaryEmployee", Employee.class);
+ 		Employee employee2 = context.getBean("secretaryEmployee", Employee.class);
+
+ 		//Close the xml file
+ 		context.close();
+
+ 		//Use myEmloyee bean
+ 		System.out.println("**Use secretaryEmployee bean");
+ 		System.out.println(employee1.getType() + "\n");
+
+ 		//true: Singelton false: Prototype
+ 		if(employee1 == employee2) {
+ 			System.out.println("**Spring uses Singelton by default"); 
+ 			System.out.println("Both objects point to the same memory address");
+ 		} else {
+ 			System.out.println("**secretaryEmployee -> @Scope(\"prototype\")"); 
+ 			System.out.println("Objects are different");
+ 		}
+ 		System.out.println(employee1);
+ 		System.out.println(employee2);
+
+ 	}
 		
-		//Ask for bean
-		Employee employee = context.getBean("secretaryEmployee", Employee.class);
-		
-		//Use init-method and destroy-method
-		System.out.println("\n" + employee.getType());
-		System.out.println("\n" + employee.getReport());
-		
-		//Close the xml file
-		context.close();
-		
-	}
 
 }
